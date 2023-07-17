1713c8f882deafd7f68715aa4e68578a12638656
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-17 18:01:43 -0400

    Create 2. hello.py

---
+++ 2. hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+# edited by Caterina Delia for practive purposes
+
+x=42
+s = print('Hello, World. {}'.format(x))
+print(s)
+
+
+
+print('Hello, World. {x}')
