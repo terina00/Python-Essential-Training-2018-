46601919c11da30f80856e1516ae0d09e7a3be67
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-17 18:08:15 -0400

    Update 2. blocks.py

--- 2. blocks.py
+++ 2. blocks.py
@@ -15,3 +15,19 @@ print('line 4')
 
 print('something else')
 print('z is {}'.format(z))
+
+#
+#
+x = 42
+y = 73
+
+if x < y:
+    print('x < y: x is {} and y is {}'.format(x, y))
+elif x > y:
+    print('x > y: x is {} and y is {}'.format(x, y))
+elif x == 5:
+    print('do five stuff')
+elif y == 8:
+    print('do 8 stuff')    
+else:
+    print('do something else')
