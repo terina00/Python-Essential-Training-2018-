cd9ea4416c6ed41e7be7725000927a7240dbb5c3
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-17 18:03:56 -0400

    Update 2. blocks.py

--- 2. blocks.py
+++ 2. blocks.py
@@ -1,1 +1,17 @@
 
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+# edited by Caterina Delia for practice purposes
+
+x = 42
+y = 73
+
+if x < y:
+    z = 122
+    print('x < y: x is {} and y is {}'.format(x, y))
+    print('line 2')
+    print('line 3')
+print('line 4')
+
+print('something else')
+print('z is {}'.format(z))
