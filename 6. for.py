9ff14a5ce77a4b474b3e84d98b121214526b9f11
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-18 15:51:44 -0400

    Create 6. for.py

---
+++ 6. for.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+# edited by Caterina Delia for practice purposes
+
+animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+for pet in animals:
+    print(pet)
+
+for pet in range(5):
+    print(pet)
