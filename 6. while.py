7699e1e22680288ac3c74b6e41ad126feece9db6
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-18 15:49:21 -0400

    Create 6. while.py

---
+++ 6. while.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+# edited by Caterina Delia for practice purposes
+
+secret = 'swordfish'
+pw = ''
+
+while pw != secret:
+    pw = input("What's the secret word? ")
