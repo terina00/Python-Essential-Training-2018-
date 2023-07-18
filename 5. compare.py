bdd12647e9b278db4201fbbcec36f85121f588de
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-18 15:32:03 -0400

    Create 5. compare.py

---
+++ 5. compare.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+# edited by Caterina Delia for practice purposes
+
+## <
+## >
+## <=
+## >=
+## ==
+## !=
+
+
+x = 42
+y = 73
+
+if x < y:
+    print('comparison is true')
+elif x > y:
+    print('x is greater than y')
+else:
+    print('comparison is false')
