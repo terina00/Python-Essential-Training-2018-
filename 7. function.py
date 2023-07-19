070a825dd1b61b5eb9d26c3e96191c44cce33ca5
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-18 21:11:25 -0400

    Update 7. function.py

--- 7. function.py
+++ 7. function.py
@@ -14,31 +14,16 @@ if __name__ == '__main__': main()
 
 ###
 
-def main():
-    kitten(5, 6, 7)
 
-def kitten(a, b = 1, c = 0):
+def main():
+    x = kitten()
+    print(type(x), x)
+
+
+def kitten():
     print('Meow.')
-    print(a, b, c)
+    return dict(x = 42, y = 43, z = 44)
+
 
 if __name__ == '__main__': main()
 
-###
-
-def main():
-    x = [5]
-    y = x
-    y[0] = 3
-    print(id(x))
-    print(id(y))
-    kitten(x)
-    print(f'in main is {x}')
-
-def kitten(a):
-    print(id(a))
-    a = 3
-    print(id(a))
-    print('Meow.')
-    print(a)
-
-if __name__ == '__main__': main()
