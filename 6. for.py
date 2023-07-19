8294472bcea3eb02f4179369aa53fd5d8c40a858
terina00 <60524595+terina00@users.noreply.github.com>
2023-07-18 20:41:33 -0400

    Update 6. for.py

--- 6. for.py
+++ 6. for.py
@@ -9,3 +9,20 @@ for pet in animals:
 
 for pet in range(5):
     print(pet)
+###
+
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+# edited by Caterina Delia for practice purposes
+
+animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+for pet in animals:
+    if pet == 'dog': continue
+    if pet == 'velociraptor': break
+    print(pet)
+else:
+    print('that is all of the animals')
+
+for pet in range(5):
+    print(pet)
